d8692c38fa51fb1f6e27e9ae4b79527bd95bfdd5
Augusto Neves <augusto.nevesti@gmail.com>
2025-12-01 01:40:00 -0300

    task #0004 - tela de detalhar usuarios

--- src/main/java/com/roomflow/bean/UsuarioBean.java
+++ src/main/java/com/roomflow/bean/UsuarioBean.java
@@ -17,6 +17,7 @@ public class UsuarioBean implements Serializable {
 
     private UsuarioService usuarioService;
     private List<Usuario> usuarios;
+    private Usuario usuarioSelecionado;
 
     public UsuarioBean() {
         this.usuarioService = new UsuarioService();
@@ -31,11 +32,23 @@ public class UsuarioBean implements Serializable {
         this.usuarios = usuarioService.getAll();
     }
 
+    public void detalharUsuario(Usuario usuario) {
+        this.usuarioSelecionado = usuario;
+    }
+
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
 
     public void setUsuarios(List<Usuario> usuarios) {
         this.usuarios = usuarios;
+    }
+
+    public Usuario getUsuarioSelecionado() {
+        return usuarioSelecionado;
+    }
+
+    public void setUsuarioSelecionado(Usuario usuarioSelecionado) {
+        this.usuarioSelecionado = usuarioSelecionado;
     }
 }
---
+++ src/main/java/com/roomflow/converter/CpfConverter.java
@@ -0,0 +1,35 @@
+package com.roomflow.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("cpfConverter")
+public class CpfConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        return value.replaceAll("[^0-9]", "");
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (value == null || value.toString().isEmpty()) {
+            return "";
+        }
+        
+        String cpf = value.toString().replaceAll("[^0-9]", "");
+        if (cpf.length() == 11) {
+            return cpf.substring(0, 3) + "." + 
+                   cpf.substring(3, 6) + "." + 
+                   cpf.substring(6, 9) + "-" + 
+                   cpf.substring(9, 11);
+        }
+        
+        return cpf;
+    }
+}
